7f9e0d7e7b13808dec7b030e5342dca3770df7e7
Huseyin Erkek <Huseyin.erk.10@gmail.com>
2025-04-28 23:01:30 +0300

    Veritabanı bağlantısı oluşturuldu

--- app.py
+++ app.py
@@ -1,24 +1,56 @@
-from flask import Flask ,render_template
+# main.py veya uygulamanızın ana dosyası
+import os
+from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask(__name__)
+# models.py dosyasındaki db ve modelleri içeri aktarıyoruz
+from models import db, Cameras, DetectionInfo
 
-@app.route('/')
-def home():  # put application's code here
+# Veritabanı adı
+DB_NAME = "dogdetection.db"
+
+# Flask uygulamasını başlatıyoruz
+DogDetec = Flask(__name__)
+
+# Flask konfigürasyonlarını ayarlıyoruz
+DogDetec.config['SECRET_KEY'] = '1234asd'
+DogDetec.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+DogDetec.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Bu ayar önerilir, gereksiz bildirimlerden kaçınmak için
+
+# SQLAlchemy bağlantısını başlatıyoruz
+db.init_app(DogDetec)
+
+# Anasayfa route
+@DogDetec.route('/')
+def home():
     return render_template('index.html')
 
-@app.route('/index.html')
+@DogDetec.route('/index.html')
 def index():
     return render_template('index.html')
-@app.route('/camera.html')
+
+@DogDetec.route('/camera.html')
 def camera():
     return render_template('camera.html')
-@app.route('/howto.html')
+
+@DogDetec.route('/howto.html')
 def howto():
     return render_template("howto.html")
 
-@app.route('/upload.html')
+@DogDetec.route('/upload.html')
 def upload():
     return render_template("upload.html")
 
+# Uygulama başlatıldığında veritabanını kontrol et
 if __name__ == '__main__':
-    app.run()
+    if not os.path.exists(DB_NAME):
+        # Flask uygulama bağlamı içinde veritabanı oluşturma işlemi
+        with DogDetec.app_context():
+            db.create_all()
+        print("Database created")
+    else:
+        print("Database already exists")
+
+    # Flask uygulamasını çalıştırıyoruz
+    DogDetec.debug = True
+    DogDetec.run()
